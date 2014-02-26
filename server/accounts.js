627d7853eb29642df03303b9cbc493fae81d7fe5
Sean Li <lishang106@gmail.com>
2014-02-26 18:08:16 -0500

    Shows info when logged in.

--- server/accounts.js
+++ server/accounts.js
@@ -1,6 +1,13 @@
 Accounts.onCreateUser(function (options, user) {
   if (options.profile) {
-    options.profile.picture = "http://graph.facebook.com/" + user.services.facebook.id + "/picture/?type=large";
+    options.profile.facebookId = user.services.facebook.id;
+    options.profile.email = user.services.facebook.email;
+    options.profile.firstName = user.services.facebook.first_name;
+    options.profile.lastName = user.services.facebook.last_name;
+    options.profile.username = user.services.facebook.username;
+    options.profile.gender = user.services.facebook.gender;
+    options.profile.price = 100
+    options.profile.bank = 1000
     user.profile = options.profile;
   }
   return user;
